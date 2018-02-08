e6861ae9d15e1119a2c2cd04d41612e16245a762
Calzaghe <serphi.256@gmail.com>
2018-02-09 00:34:04 +0100

    important fix

--- merge.cmd
+++ merge.cmd
@@ -1,34 +1,35 @@
 @ECHO OFF
 
+SET FILE_NAME=3DTryg
 COLOR 9F
-TITLE 3DTryg Merge
+TITLE %FILE_NAME% Merge
 
-IF EXIST "%CD%\3DTryg.inc" DEL /Q /A "%CD%\3DTryg.inc"
+IF EXIST "%CD%\%FILE_NAME%.inc" DEL /Q /A "%CD%\%FILE_NAME%.inc"
 IF EXIST "%CD%\errors.log" DEL /Q /A "%CD%\errors.log"
 
-FOR /F "tokens=*" %%s IN ('TYPE "%CD%\core.lst"') DO CALL :MERGE %%s
+FOR /F "tokens=*" %%s IN ('TYPE "%CD%\%FILE_NAME%.lst"') DO CALL :MERGE %%s
 
-COPY /Y "%CD%\3DTryg.inc" "%CD%\..\SAMP\include\ADM\3DTryg.inc" > nul
+COPY /Y "%CD%\%FILE_NAME%.inc" "%CD%\..\SAMP\include\ADM\%FILE_NAME%.inc" > nul
 PAUSE
 GOTO :eof
 
 :MERGE
 IF "%~1" == "import"	CALL :IMPORT "%~2"
-IF "%~1" == "write"		ECHO %~2 >> "%CD%\3DTryg.inc"
+IF "%~1" == "write"		ECHO %~2 >> "%CD%\%FILE_NAME%.inc"
 IF "%~1" == "rule"		CALL :RULE "%~2"
 IF "%~1" == "endrule"	CALL :ENDRULE
 GOTO :eof
 
 :RULE
 IF NOT EXIST "%CD%\rules\%~1.txt" GOTO :eof
-TYPE "%CD%\rules\%~1.txt" >> "%CD%\3DTryg.inc"
-ECHO. >> "%CD%\3DTryg.inc"
-ECHO. >> "%CD%\3DTryg.inc"
+TYPE "%CD%\rules\%~1.txt" >> "%CD%\%FILE_NAME%.inc"
+ECHO. >> "%CD%\%FILE_NAME%.inc"
+ECHO. >> "%CD%\%FILE_NAME%.inc"
 GOTO :eof
 
 :ENDRULE
-ECHO #endif >> "%CD%\3DTryg.inc"
-ECHO. >> "%CD%\3DTryg.inc"
+ECHO #endif >> "%CD%\%FILE_NAME%.inc"
+ECHO. >> "%CD%\%FILE_NAME%.inc"
 GOTO :eof
 
 :IMPORT
@@ -42,7 +43,7 @@ IF NOT EXIST "%CD%\%~1" (
 	GOTO :eof
 )
 ECHO Merge %~1
-TYPE "%CD%\%~1" >> "%CD%\3DTryg.inc"
-ECHO. >> "%CD%\3DTryg.inc"
-ECHO. >> "%CD%\3DTryg.inc"
+TYPE "%CD%\%~1" >> "%CD%\%FILE_NAME%.inc"
+ECHO. >> "%CD%\%FILE_NAME%.inc"
+ECHO. >> "%CD%\%FILE_NAME%.inc"
 GOTO :eof
